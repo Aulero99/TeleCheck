0fa8feb7ecdeb239b7a0de08c02bcd2b685e6fef
Auston Robertson <auston.robertson@gmail.com>
2023-09-05 23:50:31 -0600

    init

---
+++ TeleCheck/Models/Account.cs
@@ -0,0 +1,9 @@
+namespace TeleCheck.Models;
+
+public class Account
+{
+  public string Id { get; set; }
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public string Picture { get; set; }
+}
